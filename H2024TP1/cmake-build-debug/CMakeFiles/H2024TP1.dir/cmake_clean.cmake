b7323154e39e5ce71a2a368f57066d8d3ae25e6d
jaycePL <jasmin.papierz-lambert.1@ens.etsmtl.ca>
2024-01-24 19:11:51 -0500

    v0.0.0.0.0.0.1

---
+++ H2024TP1/cmake-build-debug/CMakeFiles/H2024TP1.dir/cmake_clean.cmake
@@ -0,0 +1,19 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/H2024TP1.dir/decodeur/a429.c.obj"
+  "CMakeFiles/H2024TP1.dir/decodeur/a429.c.obj.d"
+  "CMakeFiles/H2024TP1.dir/decodeur/utilitaire.c.obj"
+  "CMakeFiles/H2024TP1.dir/decodeur/utilitaire.c.obj.d"
+  "CMakeFiles/H2024TP1.dir/main.c.obj"
+  "CMakeFiles/H2024TP1.dir/main.c.obj.d"
+  "CMakeFiles/H2024TP1.dir/test.c.obj"
+  "CMakeFiles/H2024TP1.dir/test.c.obj.d"
+  "H2024TP1.exe"
+  "H2024TP1.exe.manifest"
+  "H2024TP1.pdb"
+  "libH2024TP1.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/H2024TP1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
